aa4daeeaefa40979695032e3db31e7c82d76bf6d
Azafo Alexandre Cossa <azafoalexandre@outlook.com>
2023-02-14 02:51:09 +0200

    Added navbar functionality

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,30 @@
-// document.location.reload(true);
+navToggler = document.querySelectorAll(".navbar-toggler");
+navLink = document.querySelectorAll(".nav-link");
+
+navToggler.forEach((toggler) => {
+  toggler.addEventListener("click", function () {
+    nav = document.getElementById("navbar-nav");
+    if (nav.classList.contains("show")) {
+      nav.classList.remove("show");
+
+      navLink.forEach((link) => {
+        link.addEventListener("click", () => {
+          nav.classList.remove("show");
+        });
+      });
+    } else {
+      nav.classList.add("show");
+    }
+  });
+});
+
+// button = document.getElementById(id);
+// if (button.classList.contains("btn-primary")) {
+//   button.classList.remove("btn-primary");
+//   button.textContent = "Unfollow";
+//   button.classList.add("btn-outline-primary");
+// } else {
+//   button.classList.remove("btn-outline-primary");
+//   button.textContent = "Follow";
+//   button.classList.add("btn-primary");
+// }
